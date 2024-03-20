09323ab2383798b2817cb06161dc38402c0f5642
commandctrl <77082944+commandctrl@users.noreply.github.com>
2024-03-20 12:34:21 -0700

    Update ipv4_cidr_rDNS.py

--- ipv4_cidr_rDNS.py
+++ ipv4_cidr_rDNS.py
@@ -3,13 +3,16 @@ import socket
 import time
 import csv
 
+# defang domains with [.]
 def defang(domain):
     return domain.replace('.','[.]')
 
+# cidr to run rDNS
 cidr_block = '77.90.185.0/24'
 
 ip_addresses = [str(ip) for ip in ipaddress.ip_network(cidr_block)]
 
+# csv file to export results
 csv_file = '/Users/<user>/Documents/rDNS_lookup_results.csv'
 with open(csv_file, mode='w', newline='') as file:
     writer = csv.writer(file)
